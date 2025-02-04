cb66ecd9035a3ee79e17c73378d974e8e9855e89
Manzar Nazar <142090562+manzarnazar@users.noreply.github.com>
2025-02-04 18:06:25 +0500

    test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,21 +22,6 @@
     </head>
     <body class="font-Fraunces">
         <p style="font-weight: 400;">This is a normal weight text.</p>
-<p style="font-weight: 700;">This is a bold weight text.</p>
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
 
-            <!-- Page Heading -->
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
     </body>
 </html>
