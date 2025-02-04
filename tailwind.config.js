39f3cf453d94ccad7871e457974e788a241a997d
Manzar Nazar <142090562+manzarnazar@users.noreply.github.com>
2025-02-04 16:54:08 +0500

    font change

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                serif: ['Fraunces', 'serif'],
+                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
         },
     },
