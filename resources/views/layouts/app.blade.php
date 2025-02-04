9e7fc238207dc0947119e7008b4db21f3c9f728e
Manzar Nazar <142090562+manzarnazar@users.noreply.github.com>
2025-02-04 16:50:57 +0500

    font change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,18 +4,18 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <link href="https://fonts.googleapis.com/css2?family=Fraunces:wght@400;500;700&display=swap" rel="stylesheet">
-
         <title>{{ config('app.name', get_settings('system_name')) }}</title>
-
+        
         <!-- Fonts -->
-       
-
+        
+        
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-
+        
+        
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
+        <link href="https://fonts.googleapis.com/css2?family=Fraunces:wght@400;500;700&display=swap" rel="stylesheet">
     </head>
     <body class="font-serif">
         <div class="min-h-screen bg-gray-100">
